d02669b6ab265586680a90176ba40070123404be
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-08-31 18:50:22 +0530

    connecting-to-database

---
+++ config/dbConnect.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const dbConnect = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+
+    const connected = await mongoose.connect(process.env.MONGO_URL);
+
+    console.log(`MongoDB connected ${connected.connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+dbConnect();
