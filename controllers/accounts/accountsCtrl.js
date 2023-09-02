7d38a1de139b906e3b98671064058564d0db17b5
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-09-02 18:28:05 +0530

    create-account

---
+++ controllers/accounts/accountsCtrl.js
@@ -0,0 +1,46 @@
+const Account = require("../../model/Account");
+const User = require("../../model/User");
+const { appErr } = require("../../utils/appErr");
+
+// ! create
+const createAccountCtrl = async (request, response, next) => {
+  const { name, initialBalance, accountType, notes } = request.body;
+  try {
+    // TODO 01: Find the loggedIn user
+    const userFound = await User.findById(request.user);
+    if (!userFound) {
+      return next(appErr("User not found", 404));
+    }
+
+    // TODO 02: Create the account
+    const account = await Account.create({
+      name,
+      initialBalance,
+      accountType,
+      notes,
+      createdBy: request.user,
+    });
+
+    // TODO 03: Push the account into users accounts field
+    userFound.accounts.push(account._id);
+
+    // TODO 04: Resave the user
+    await userFound.save();
+
+    // ? send the response
+    response.json({
+      status: "success",
+      data: account,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  createAccountCtrl,
+  getAccountCtrl,
+  deleteAccountCtrl,
+  updateAccountCtrl,
+  getAccountsCtrl,
+};
